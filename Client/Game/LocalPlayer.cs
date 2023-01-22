227a9e864a189521cb79ac5006ee77a181cbe31b
voidptr_t <39582793+Sanceilaks@users.noreply.github.com>
2023-01-22 19:56:27 +0300

    Init

--- Client/Game/LocalPlayer.cs
+++ Client/Game/LocalPlayer.cs
@@ -8,12 +8,13 @@ namespace Client.Game;
 public class LocalPlayer : Player
 {
 	public Texture2D Sprite { get; set;}
+	public SpriteFont Font { get; set; }
 	public Vector2 Size { get; set;}
-
 	public Vector2 Velocity { get; set; } = Vector2.Zero;
 	
-	public Vector2 Speed { get; set; } = new Vector2(100f, 100f);
-	public Vector2 MaxSpeed { get; set; } = new Vector2(2000f, 2000f);
+	public Vector2 Speed { get; set; } = new Vector2(10f, 10f);
+	public Vector2 MaxSpeed { get; set; } = new Vector2(3000f, 3000f);
+	public Vector2 MaxWalkSpeed { get; set; } = new Vector2(50f, 50f);	
 
 	public LocalPlayer(Microsoft.Xna.Framework.Game game) : base(game)
 	{
@@ -21,45 +22,27 @@ public class LocalPlayer : Player
 		Size = new Vector2(40, 60);
 	}
 
-	public static Vector2 NormalizeVelocity(Vector2 velocity, Vector2 maxVelocity, float deltaTime)
-	{
-		if (Math.Abs(velocity.X) > maxVelocity.X)
-		{
-			velocity.X -= Math.Sign(velocity.X) * (Math.Abs(velocity.X) - maxVelocity.X) * deltaTime;
-		}
-		
-		if (Math.Abs(velocity.Y) > maxVelocity.Y)
-		{
-			velocity.Y -= Math.Sign(velocity.Y) * (Math.Abs(velocity.Y) - maxVelocity.Y) * deltaTime;
-		}
-
-		return velocity;
-	}
-
-	public static Vector2 SubstractVelocity(Vector2 velocity, float deltaTime, Vector2 speed)
-	{
-		velocity.X -= Math.Sign(velocity.X) * deltaTime * speed.X;
-		velocity.Y -= Math.Sign(velocity.Y) * deltaTime * speed.Y;
-		return velocity;
-	}
-
 	public override void Update(GameTime gameTime)
 	{
 		var delta = (float)gameTime.ElapsedGameTime.TotalSeconds;
 		if (GamePad.GetState(PlayerIndex.One).DPad.Down == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.S))
-			Velocity += Velocity.Y <= MaxSpeed.Y ? Vector2.UnitY * Speed.Y * delta : Vector2.Zero;
+			Velocity += Velocity.Y <= MaxWalkSpeed.Y ? Vector2.UnitY * Speed.Y * delta : Vector2.Zero;
 		if (GamePad.GetState(PlayerIndex.One).DPad.Up == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.W))
-			Velocity -= Velocity.Y <= MaxSpeed.Y ? Vector2.UnitY * Speed.Y * delta : Vector2.Zero;
+			Velocity -= Velocity.Y <= MaxWalkSpeed.Y ? Vector2.UnitY * Speed.Y * delta : Vector2.Zero;
 		if (GamePad.GetState(PlayerIndex.One).DPad.Right == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.D))
-			Velocity += Velocity.X <= MaxSpeed.X ? Vector2.UnitX * Speed.X * delta : Vector2.Zero;
+			Velocity += Velocity.X <= MaxWalkSpeed.X ? Vector2.UnitX * Speed.X * delta : Vector2.Zero;
 		if (GamePad.GetState(PlayerIndex.One).DPad.Left == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.A))
-			Velocity -= Velocity.X <= MaxSpeed.X ? Vector2.UnitX * Speed.X * delta : Vector2.Zero;
+			Velocity -= Velocity.X <= MaxWalkSpeed.X ? Vector2.UnitX * Speed.X * delta : Vector2.Zero;
 		
 
-		Velocity = NormalizeVelocity(Velocity, MaxSpeed, delta);
-		Position += Velocity;
+		Velocity -= Velocity * 2f * delta;
 
-		Velocity = SubstractVelocity(Velocity, delta, Speed);
+		var vel = Velocity;
+		vel.X = MathHelper.Clamp(vel.X, -MaxSpeed.X, MaxSpeed.X);
+		vel.Y = MathHelper.Clamp(vel.Y, -MaxSpeed.Y, MaxSpeed.Y);
+		Velocity = vel;
+
+		Position += Velocity;
 
 		base.Update(gameTime);
 	}
@@ -68,6 +51,7 @@ public class LocalPlayer : Player
 	{
 		var content = Game.Content;
 		Sprite = content.Load<Texture2D>("Textures/Player");
+		Font = content.Load<SpriteFont>("Fonts/Font");
 		base.LoadContent();
 	}
 
@@ -75,7 +59,16 @@ public class LocalPlayer : Player
 	{
 		spriteBatch.Begin(transformMatrix: Game1.GetCamera(Game).Transform);
 		spriteBatch.Draw(Sprite, new Rectangle((int)Position.X, (int)Position.Y, (int)Size.X, (int)Size.Y), Color.White);
+		spriteBatch.DrawString(Font, "Vazgen", new Vector2(
+			(int)Position.X + Size.X / 2 - Font.MeasureString("Vazgen").X * 0.5f, 
+			(int)Position.Y - Font.MeasureString("Vazgen").Y), Color.White
+		);
 		spriteBatch.End();
+		
+		spriteBatch.Begin();
+		spriteBatch.DrawString(Font, Vector2.Floor(Position).ToString(), new Vector2(0, 0), Color.White);
+		spriteBatch.End();
+
 
 		base.Draw(gameTime);
 	}
